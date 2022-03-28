1701bf8a56179861b70009cc17c338cac49fb3ce
Sruthi <sruthiprabhendu@gmail.com>
2022-03-28 18:17:37 +0200

    added more tests

--- app/src/test/java/PersonalExpenseManager/ExpenseDataRepositoryTest.java
+++ app/src/test/java/PersonalExpenseManager/ExpenseDataRepositoryTest.java
@@ -4,8 +4,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ExpenseDataRepositoryTest {
 
@@ -16,17 +18,37 @@ public class ExpenseDataRepositoryTest {
 
     }
 
+    //Requirement 1: Data should be saved to the database.
+
     @Test
     public void shouldAddDataToTheDatabase() {
 
         Double amount = 150.00;
-        ExpenseData expenseData = new ExpenseData("Netflix",LocalDate.now(),amount);
-        expenseDataRepository.create(expenseData); // insert data into database
-        ExpenseData result = expenseDataRepository.getExpenseData().get(0); // Get data from database since its an array get the first item
-        Double actualAmount = result.getAmount(); // amount from the
+        LocalDate date= LocalDate.now();
+        String dataType="Netflix";
 
-        assertEquals("Amount should be correct",amount, actualAmount);
+        ExpenseData expenseData = new ExpenseData(dataType,date,amount);
+        ExpenseData expenseData1 = expenseDataRepository.create(expenseData);// insert data into database
+
+        assertEquals("Amount should be correct",amount, expenseData1.getAmount());
+        assertEquals("Date should be correct",date, expenseData1.getDate());
+        assertEquals("DataType should be correct",dataType, expenseData1.getExpenseType());
+
     }
 
+    // Regquirement 2: Should be able to fetch all data from database
+
+    @Test
+    public void shouldBeAbleToRetrieveAllData(){
+
+        Double amount = 150.00;
+        LocalDate date= LocalDate.now();
+        String dataType="Netflix";
+
+        ExpenseData expenseData = new ExpenseData(dataType,date,amount);
+        expenseDataRepository.create(expenseData);// insert data into database
+        List<ExpenseData> result = expenseDataRepository.getExpenseData(); // Get data from database since it's an array get the first item
+    assertTrue("Array size should be greater than Zero",result.size()>0);
+    }
 
 }
